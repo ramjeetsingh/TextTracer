526dc11abd9a66ac674523a96d163a4e7840f354
Ramjeet Singh <ramjeet2k2@gmail.com>
2023-11-14 12:00:21 +0530

    implemented preprocessing

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 import cv2
 import pytesseract
 from pytesseract import Output
+import numpy as np
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-img = cv2.imread('test2.jpg')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)    #bcoz cv2 reads rgb images and pytesseract works with only bgr values
+
+
+image = cv2.imread('test3.png')
+gray_image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)             #bcoz cv2 reads rgb images and pytesseract works with only bgr values
+thresh, binary = cv2.threshold(gray_image, 180, 255, cv2.THRESH_BINARY)
+
+
+strct_element=cv2.getStructuringElement(cv2.MORPH_RECT , (5,5))
+bg=cv2.morphologyEx(binary, cv2.MORPH_DILATE, strct_element)
+img=cv2.divide(binary, bg, scale=255)
+
 
 print(pytesseract.image_to_string(img))
 
+
 myconfig = r"--psm 11 --oem 3"
 
-# 2) to get boxes around words
 
 data = pytesseract.image_to_data(img, config=myconfig, output_type=Output.DICT)
 no_boxes = len(data['text'])
 
+
 for i in range(no_boxes):
-    if float(data['conf'][i]) > 50:                #if confidence is > 50
+    if float(data['conf'][i]) > 50:                             #if confidence is > 50
         (x, y, width, height) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
         img = cv2.rectangle(img, (x, y), (x + width, y + height), (0, 255, 0), 2)
         img = cv2.putText(img, data['text'][i], (x, y + height + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+
 
 cv2.imshow('Result', img)
 cv2.waitKey(0)
--- test.py
+++ test.py
@@ -1,28 +1,20 @@
-import requests
-from bs4 import BeautifulSoup
+import cv2
+import numpy as np
+import pytesseract as pt
 
-# Define the URL of the webpage you want to scrape
-url = 'https://ramjeetsingh.github.io/Drum-Kit/'
+image = cv2.imread('test5.jpg')
+gray_image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
-# Send an HTTP GET request to the URL
-response = requests.get(url)
+strct_element=cv2.getStructuringElement(cv2.MORPH_RECT , (8,8))
+bg=cv2.morphologyEx(gray_image, cv2.MORPH_DILATE, strct_element)
+im_gray=cv2.divide(gray_image, bg, scale=255)
 
-# Check if the request was successful (status code 200)
-if response.status_code == 200:
-    # Parse the HTML content of the webpage using BeautifulSoup
-    soup = BeautifulSoup(response.text, 'html.parser')
+kernel = np.ones((1,1), np.uint8)
+result = cv2.dilate(im_gray, kernel, iterations=1)
 
-    # Define a function to print the element type and its content
-    def print_element_type_and_content(element):
-        element_type = element.name  # Get the HTML tag name
-        element_content = element.get_text()  # Get the text content within the element
-        print(f"Element Type: {element_type}")
-        print(f"Element Content: {element_content}")
-        print("\n")
+cv2.imshow('res', result)
+cv2.waitKey(0)
 
-    # Find all HTML elements in the soup and print their type and content
-    all_elements = soup.find_all()
-    for element in all_elements:
-        print_element_type_and_content(element)
-else:
-    print('Failed to retrieve the webpage')
+# ocr_result = pt.image_to_string(no_noise)
+# print(ocr_result)
+
